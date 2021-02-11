fb7bf6b64d66ef0d6ce2da35e5fe723e76717edb
xamogast <ajz@lambd.ax>
2021-02-11 20:45:55 +0100

    Update python.nix

--- python.nix
+++ python.nix
@@ -3,7 +3,8 @@ let
   python = pkgs.python37;
 in
 python.withPackages (ps: with ps; [
-  django_2_2
+  #django_2_2
+  Django
   whitenoise    # for serving static files
   brotli        # brotli compression for whitenoise
   gunicorn      # for serving via http
